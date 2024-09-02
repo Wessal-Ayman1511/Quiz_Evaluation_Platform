1ba56b8215701b3c47b0edf3b27f5b51fe0e48e2
Wessal Ayman <wessalayman237@gmail.com>
2024-09-03 00:11:26 +0300

    updated login

--- app/api/routes/login.py
+++ app/api/routes/login.py
@@ -8,24 +8,30 @@ from datetime import timedelta
 @app_views.route('/api/login', methods=['POST'])
 def login():
     data = request.get_json()
+
     if not data:
         return jsonify({'error': 'Invalid input format or no input data provided'}), 400
 
-    name = data.get('name')
+    email = data.get('email')
     password = data.get('password')
-    role = data.get('role')
 
-    if not name or not password or not role:
-        return jsonify({'error': 'Missing name, password, or role'}), 400
-    if len(name) < 2 or len(name) > 50:
-        return jsonify({'error': 'Name must be between 2 and 50 characters'}), 400
+    if not email or not password:
+        return jsonify({'error': 'Missing email or password'}), 400
+
+    if len(email) < 5 or len(email) > 100:
+        return jsonify({'error': 'Email must be between 5 and 100 characters'}), 400
+
     if len(password) < 8:
         return jsonify({'error': 'Password must be at least 8 characters long'}), 400
 
-    user = User.query.filter_by(name=name, role=role).first()
+    user = User.query.filter_by(email=email).first()
 
     if user and check_password_hash(user.password, password):
-        access_token = create_access_token(identity=user.id, expires_delta=timedelta(hours=2), additional_claims={"role": user.role})  
+        access_token = create_access_token(
+            identity=user.id,
+            expires_delta=timedelta(hours=2),
+            additional_claims={"role": user.role}
+        )
         return jsonify({'message': 'Login successful', 'role': user.role, 'access_token': access_token}), 200
     else:
-        return jsonify({'error': 'Invalid username, password, or role'}), 401
+        return jsonify({'error': 'Invalid email or password'}), 401
