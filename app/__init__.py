8a9f4925ba5f7610e1c2a8fc49b14d60b009f66c
WeNny5 <abdelfatahwenny@yahoo.com>
2024-09-12 13:52:49 +0300

    added CORS to the init

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('app.config.Config')
 
     db.init_app(app)
